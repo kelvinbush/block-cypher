ed9e52d7f9ababeaf02ba11329779b85d0b96c2e
Kelvin Bush <kelybush@gmail.com>
2022-06-20 20:24:53 +0300

    set up chart container skeleton with lifecycle

--- src/components/ChartContainer.jsx
+++ src/components/ChartContainer.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Chart from "./Chart";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchChartData} from "../redux/charts/actions";
 
-const ChartContainer = () => {
+const ChartContainer = ({coinId}) => {
+  const [days, setDays] = useState("1");
+  const {charts} = useSelector(state => state.charts);
+  const {loading, error} = charts;
+  const dispatch = useDispatch();
+  const daysOptions = [1, 7, 30, 180, 365];
+
+  useEffect(() => {
+    dispatch(fetchChartData(coinId, days));
+  }, [dispatch, coinId, days]);
+
   return (
-    <>
-      <Chart />
-    </>)
+    <div>
+      <h1>Chart</h1>
+      <p>This is the chart page</p>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {charts.prices && <Chart prices={charts.prices} days={days}/>}
+      <select value={days} onChange={(e) => setDays(e.target.value)}>
+        {daysOptions.map((item) => <option key={item} value={item}>{item} days</option>)}
+      </select>
+    </div>
+  );
 };
 
 export default ChartContainer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = (props) => (
   <div>
     <h1>Home</h1>
     <p>This is the home page</p>
-    <ChartContainer />
+    <ChartContainer coinId={'bitcoin'}/>
   </div>
 );
 
