d0cfef2b20b5a18498753d1ca22121fc177b402f
Kelvin Bush <kelybush@gmail.com>
2022-06-20 19:29:56 +0300

    set up chart actions

---
+++ src/redux/charts/actions.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const baseUrl = "https://api.coingecko.com/api/v3/coins/";
+const FETCH_CHART_DATA = 'chartsStore/charts/FETCH_CHART_DATA';
+const FETCH_CHART_DATA_ERROR = 'chartsStore/charts/FETCH_CHART_DATA_ERROR';
+const FETCH_CHART_DATA_LOADING = 'chartsStore/charts/FETCH_CHART_DATA_LOADING';
+
+
+const fetchChartDataAction = (prices) => ({
+  type: FETCH_CHART_DATA,
+  payload: prices
+});
+
+const fetchChartDataErrorAction = (error) => ({
+  type: FETCH_CHART_DATA_ERROR,
+  payload: error
+});
+
+const fetchChartDataLoading = () => ({
+  type: FETCH_CHART_DATA_LOADING
+});
+
+
+export const fetchChartData = (coinId, days) => async (dispatch) => {
+  dispatch(fetchChartDataLoading());
+  try {
+    const response = await axios.get(`${baseUrl}${coinId}/market_chart?vs_currency=usd&days=${days}`);
+    dispatch(fetchChartDataAction(response.data.prices));
+  } catch (error) {
+    dispatch(fetchChartDataErrorAction(error));
+  }
+}
