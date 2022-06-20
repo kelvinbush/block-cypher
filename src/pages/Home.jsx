6eac3c8ff9caed20ad431c5b56675131910594e0
Kelvin Bush <kelybush@gmail.com>
2022-06-20 20:11:48 +0300

    rename files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChartContainer from "../components/ChartContainer";
+
+const Home = (props) => (
+  <div>
+    <h1>Home</h1>
+    <p>This is the home page</p>
+    <ChartContainer />
+  </div>
+);
+
+export default Home;
