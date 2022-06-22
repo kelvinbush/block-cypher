97f23b9ffcb545fd9ba33dcdea2f3e9a0d7ebb43
Kelvin Bush <kelybush@gmail.com>
2022-06-22 09:49:19 +0300

    test components

--- src/components/__tests__/Components.test.js
+++ src/components/__tests__/Components.test.js
@@ -2,16 +2,49 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import ChangeIndicator from '../ChangeIndicator/ChangeIndicator';
+import Chart from '../Chart/Chart';
+import CoinList from '../CoinList/CoinList';
+import Hero from '../Hero/Hero';
 
-describe('Test presence of ChangeIndicator', () => {
-  test('should render Navbar component', () => {
-    const navComponent = renderer
+describe('Test presence of Components', () => {
+  it('should render Navbar component', () => {
+    const component = renderer
       .create(
         <BrowserRouter>
-          <ChangeIndicator />
+          <ChangeIndicator coinData={{}} isList={true} />
         </BrowserRouter>,
       )
       .toJSON();
-    expect(navComponent).toMatchSnapshot();
+    expect(component).toMatchSnapshot();
+  });
+  it('should render Chart component', () => {
+    const component = renderer
+      .create(
+        <BrowserRouter>
+          <Chart prices={[]} days={'1'} />
+        </BrowserRouter>,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+  it('should render Hero component', () => {
+    const component = renderer
+      .create(
+        <BrowserRouter>
+          <Hero />
+        </BrowserRouter>,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+  it('should render CoinList component', () => {
+    const component = renderer
+      .create(
+        <BrowserRouter>
+          <CoinList markets={[]} loading={false} />
+        </BrowserRouter>,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
   });
 });
